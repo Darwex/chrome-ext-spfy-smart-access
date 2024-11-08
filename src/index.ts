9ad04f802a55bd2f7b818ca0579add29831fdb7b
David Vrsek <david.vrsek@gmail.com>
2024-11-08 17:23:36 +0100

    "Added Typescript and Vite"

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+chrome.runtime.onInstalled.addListener(() => {
+    console.info("Quick ACL for Shopify Installed");
+  });
+
+chrome.runtime.onStartup.addListener(() => {
+    console.info("Quick ACL for Shopify Started");
+});
