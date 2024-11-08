1aa5162455ac8c611a0df38376ac158a4bd81527
David Vrsek <david.vrsek@gmail.com>
2024-11-08 17:47:44 +0100

    Added: eslint and prettier support

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 chrome.runtime.onInstalled.addListener(() => {
-    console.info("Quick ACL for Shopify Installed");
-  });
+    console.info('Quick ACL for Shopify Installed');
+});
 
 chrome.runtime.onStartup.addListener(() => {
-    console.info("Quick ACL for Shopify Started");
+    console.info('Quick ACL for Shopify Started');
+    console.log('aa');
 });
